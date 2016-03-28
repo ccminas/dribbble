53a482945cea1c9af01867eae883d4c6adba9a5b
Day <dayannefernandesc@gmail.com>
2016-03-27 22:48:14 -0300

    Adicionando arquivo main e a bib settings.

---
+++ maq-maq-uno/maq-maq-uno.ino
@@ -0,0 +1,376 @@
+/*
+ ************************************************
+ ************** MAKEY MAKEY *********************
+ ************************************************
+ 
+ /////////////////////////////////////////////////
+ /////////////HOW TO EDIT THE KEYS ///////////////
+ /////////////////////////////////////////////////
+ - Edit keys in the settings.h file
+ - that file should be open in a tab above (in Arduino IDE)
+ - more instructions are in that file
+ 
+ //////////////////////////////////////////////////
+ ///////// MaKey MaKey FIRMWARE v1.4.1 ////////////
+ //////////////////////////////////////////////////
+ by: Eric Rosenbaum, Jay Silver, and Jim Lindblom
+ MIT Media Lab & Sparkfun
+ start date: 2/16/2012 
+ current release: 7/5/2012
+ 
+ Portado para o Arduino UNO em 09/05/2013
+ por Daniel O. Basconcello Filho
+ 
+ Verificar a forma de configurar na linha 46
+ Verificar a saida pela porta serial nas linhas 341 e 363
+ 
+ 
+ */
+
+/////////////////////////
+// DEBUG DEFINITIONS ////               
+/////////////////////////
+//#define DEBUG
+//#define DEBUG2 
+//#define DEBUG3 
+//#define DEBUG_TIMING
+//#define DEBUG_MOUSE
+//#define DEBUG_TIMING2
+
+////////////////////////
+// DEFINED CONSTANTS////
+////////////////////////
+
+#define BUFFER_LENGTH    3     // 3 bytes gives us 24 samples
+#define NUM_INPUTS       4    // 6 on the front + 12 on the back
+
+//Pinos ativos
+//Indicar aqui quais pinos serao usados no shield
+int pinNumbers[NUM_INPUTS] = {
+  
+ // A0,A1,A2,A3,A4,A5,
+  3,4,5,6
+  
+};
+
+//Pinos no shield
+//  6,5,4,3
+//  7,8,9,10,11,12,
+//  A0,A1,A2,A3,A4,A5
+
+
+#define TARGET_LOOP_TIME 744  // (1/56 seconds) / 24 samples = 744 microseconds per sample 
+
+// id numbers for mouse movement inputs (used in settings.h)
+#define MOUSE_MOVE_UP       -1 
+#define MOUSE_MOVE_DOWN     -2
+#define MOUSE_MOVE_LEFT     -3
+#define MOUSE_MOVE_RIGHT    -4
+
+//#define DEBUG
+
+#define led_pin   13
+
+#include "settings.h"
+
+/////////////////////////
+// STRUCT ///////////////
+/////////////////////////
+typedef struct {
+  byte pinNumber;
+  int keyCode;
+  byte measurementBuffer[BUFFER_LENGTH]; 
+  boolean oldestMeasurement;
+  byte bufferSum;
+  boolean pressed;
+  boolean prevPressed;
+  boolean isMouseMotion;
+  boolean isMouseButton;
+  boolean isKey;
+} 
+MakeyMakeyInput;
+
+MakeyMakeyInput inputs[NUM_INPUTS];
+
+///////////////////////////////////
+// VARIABLES //////////////////////
+///////////////////////////////////
+int bufferIndex = 0;
+byte byteCounter = 0;
+byte bitCounter = 0;
+int mouseMovementCounter = 0; // for sending mouse movement events at a slower interval
+
+int pressThreshold;
+int releaseThreshold;
+boolean inputChanged;
+
+int mouseHoldCount[NUM_INPUTS]; // used to store mouse movement hold data
+
+
+//input status LED pin numbers
+//const int inputLED_a = 9;
+//const int inputLED_b = 10;
+//const int inputLED_c = 11;
+//const int outputK = 14;
+//const int outputM = 16;
+//byte ledCycleCounter = 0;
+
+// timing
+int loopTime = 0;
+int prevTime = 0;
+int loopCounter = 0;
+
+
+///////////////////////////
+// FUNCTIONS //////////////
+///////////////////////////
+void initializeArduino();
+void initializeInputs();
+void updateMeasurementBuffers();
+void updateBufferSums();
+void updateBufferIndex();
+void updateInputStates();
+void addDelay();
+void updateOutLEDs();
+
+//////////////////////
+// SETUP /////////////
+//////////////////////
+void setup() 
+{
+  initializeArduino();
+  initializeInputs();
+}
+
+////////////////////
+// MAIN LOOP ///////
+////////////////////
+void loop() 
+{
+  updateMeasurementBuffers();
+  updateBufferSums();
+  updateBufferIndex();
+  updateInputStates();
+  updateOutLEDs();
+  addDelay();
+}
+
+//////////////////////////
+// INITIALIZE ARDUINO
+//////////////////////////
+void initializeArduino() {
+
+  Serial.begin(9600);  // Serial for debugging
+
+  /* Set up input pins 
+   DEactivate the internal pull-ups, since we're using external resistors */
+  for (int i=0; i<NUM_INPUTS; i++)
+  {
+    pinMode(pinNumbers[i], INPUT);
+    digitalWrite(pinNumbers[i], LOW);
+  }
+
+  pinMode(led_pin, OUTPUT);
+
+}
+
+///////////////////////////
+// INITIALIZE INPUTS
+///////////////////////////
+void initializeInputs() {
+
+  float thresholdPerc = SWITCH_THRESHOLD_OFFSET_PERC;
+  float thresholdCenterBias = SWITCH_THRESHOLD_CENTER_BIAS/50.0;
+  float pressThresholdAmount = (BUFFER_LENGTH * 8) * (thresholdPerc / 100.0);
+  float thresholdCenter = ( (BUFFER_LENGTH * 8) / 2.0 ) * (thresholdCenterBias);
+  pressThreshold = int(thresholdCenter + pressThresholdAmount);
+  releaseThreshold = int(thresholdCenter - pressThresholdAmount);
+
+  for (int i=0; i<NUM_INPUTS; i++) {
+    inputs[i].pinNumber = pinNumbers[i];
+    inputs[i].keyCode = keyCodes[i];
+
+    for (int j=0; j<BUFFER_LENGTH; j++) {
+      inputs[i].measurementBuffer[j] = 0;
+    }
+    
+    inputs[i].oldestMeasurement = 0;
+    inputs[i].bufferSum = 0;
+
+    inputs[i].pressed = false;
+    inputs[i].prevPressed = false;
+
+    inputs[i].isMouseMotion = false;
+    inputs[i].isMouseButton = false;
+
+    inputs[i].isKey = true;
+
+  }
+}
+
+
+//////////////////////////////
+// UPDATE MEASUREMENT BUFFERS
+//////////////////////////////
+void updateMeasurementBuffers() {
+
+  for (int i=0; i<NUM_INPUTS; i++) {
+
+    // store the oldest measurement, which is the one at the current index,
+    // before we update it to the new one 
+    // we use oldest measurement in updateBufferSums
+    byte currentByte = inputs[i].measurementBuffer[byteCounter];
+    inputs[i].oldestMeasurement = (currentByte >> bitCounter) & 0x01; 
+
+    // make the new measurement
+    boolean newMeasurement = digitalRead(inputs[i].pinNumber);
+
+    // invert so that true means the switch is closed
+    newMeasurement = !newMeasurement; 
+
+    // store it    
+    if (newMeasurement) {
+      currentByte |= (1<<bitCounter);
+    } 
+    else {
+      currentByte &= ~(1<<bitCounter);
+    }
+    
+    inputs[i].measurementBuffer[byteCounter] = currentByte;
+  }
+}
+
+///////////////////////////
+// UPDATE BUFFER SUMS
+///////////////////////////
+void updateBufferSums() {
+
+  // the bufferSum is a running tally of the entire measurementBuffer
+  // add the new measurement and subtract the old one
+
+  for (int i=0; i<NUM_INPUTS; i++) {
+    byte currentByte = inputs[i].measurementBuffer[byteCounter];
+    boolean currentMeasurement = (currentByte >> bitCounter) & 0x01; 
+    if (currentMeasurement) {
+      inputs[i].bufferSum++;
+    }
+    if (inputs[i].oldestMeasurement) {
+      inputs[i].bufferSum--;
+    }
+  }  
+}
+
+///////////////////////////
+// UPDATE BUFFER INDEX
+///////////////////////////
+void updateBufferIndex() {
+  bitCounter++;
+  if (bitCounter == 8) {
+    bitCounter = 0;
+    byteCounter++;
+    if (byteCounter == BUFFER_LENGTH) {
+      byteCounter = 0;
+    }
+  }
+}
+
+///////////////////////////
+// UPDATE INPUT STATES
+///////////////////////////
+void updateInputStates() {
+  inputChanged = false;
+  for (int i=0; i<NUM_INPUTS; i++) {
+    inputs[i].prevPressed = inputs[i].pressed; // store previous pressed state (only used for mouse buttons)
+    if (inputs[i].pressed) {
+      if (inputs[i].bufferSum < releaseThreshold) {  
+        inputChanged = true;
+        inputs[i].pressed = false;
+        
+        if (inputs[i].isKey) {
+          
+          //Tecla solta - envio pela porta serial
+          //o numero da tecla no vetor de teclas seguido de
+          //um numero 0 que indica que a tecla foi solta
+          //Serial.print(i);
+          if(i == 0){ Serial.print("ESQUERDA "); }
+          else if(i == 1){ Serial.print("CIMA "); }
+          else if(i == 2){ Serial.print("BAIXO "); }
+          else{ Serial.print("DIREITA "); }
+          Serial.println("SOLTOU");
+        }
+        if (inputs[i].isMouseMotion) {  
+          mouseHoldCount[i] = 0;  // input becomes released, reset mouse hold
+        }
+      }
+      else if (inputs[i].isMouseMotion) {  
+        mouseHoldCount[i]++; // input remains pressed, increment mouse hold
+      }
+    } 
+    else if (!inputs[i].pressed) {
+      if (inputs[i].bufferSum > pressThreshold) {  // input becomes pressed
+      
+        inputChanged = true;
+        inputs[i].pressed = true; 
+
+        if (inputs[i].isKey) {
+          //Tecla pressionada - envio pela porta serial
+          // SEGURANDO
+          Serial.println("SEGURANDO");
+        }
+      }
+    }
+  }
+}
+
+
+///////////////////////////
+// ADD DELAY
+///////////////////////////
+void addDelay() {
+
+  loopTime = micros() - prevTime;
+  if (loopTime < TARGET_LOOP_TIME) {
+    int wait = TARGET_LOOP_TIME - loopTime;
+    delayMicroseconds(wait);
+  }
+
+  prevTime = micros();
+
+}
+
+void updateOutLEDs()
+{
+  boolean keyPressed = 0;
+  boolean mousePressed = 0;
+
+  for (int i=0; i<NUM_INPUTS; i++)
+  {
+    if (inputs[i].pressed)
+    {
+      if (inputs[i].isKey)
+      {
+        keyPressed = 1;
+      }
+      else
+      {
+        mousePressed = 1;
+      }
+    }
+  }
+
+  if (keyPressed)
+  {
+    digitalWrite( led_pin, HIGH);
+    
+  }
+  else
+  {
+    digitalWrite( led_pin, LOW);
+  }
+}
+
+
+
+
+
+
